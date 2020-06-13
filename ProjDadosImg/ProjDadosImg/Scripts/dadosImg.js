ab9220e1784f560b7c8736f17ac96675bc7686f3
gilivasconcelos <gilivasconcelos@hotmail.com>
2020-06-13 06:36:47 -0300

    Add files via upload

---
+++ ProjDadosImg/ProjDadosImg/Scripts/dadosImg.js
@@ -0,0 +1,139 @@
+﻿
+
+
+jQuery(document).ready(function ($) {
+
+    // All your code using $
+    $(document).ready(function () {
+        BuscarDadosImg();
+    });
+
+});
+
+
+//Funcao BuscarDadosImg
+function BuscarDadosImg() {
+    $.ajax({
+        url: "/Home/ListarDadosImg",
+        type: "GET",
+        contentType: "application/json;charset=utf-8",
+        dataType: "json",
+        success: function (result) {
+            var html = '';
+            $.each(result, function (key, item) {
+                html += '<tr>';
+                html += '<td>' + item.IdDadosImg + '</td>';
+                html += '<td>' + item.FileSizeBytes + '</td>';
+                html += '<td>' + item.Url + '</td>';
+                html += '<td>' + item.Descricao + '</td>';
+                html += '<td><a href="#"  onclick="return buscarDadosImgPorId(' + item.IdDadosImg + ')">Editar</a> | <a href="#" onclick="deletarDadosImg(' + item.IdDadosImg + ')">Deletar</a></td>';
+                html += '</tr>';
+            });
+            $('.tbody').html(html);
+        },
+        error: function (errormessage) {
+            alert(errormessage.responseText);
+        }
+    });
+}
+
+
+//Add novo DadosImg
+function inserirDadosImg() {
+
+    var dadosImgObj = {
+        FileSizeBytes: $('#FileSizeBytes').val(),
+        Url: $('#Url').val(),
+        Descricao: $('#Descricao').val()
+    };
+    $.ajax({
+        url: "/Home/AddNewDadosImg",
+        data: JSON.stringify(dadosImgObj),
+        type: "POST",
+        contentType: "application/json;charset=utf-8",
+        dataType: "json",
+        success: function (result) {
+            buscarDadosIgm();
+            $('#ModalAdicionar').modal('hide');
+        },
+        error: function (errormessage) {
+            alert(errormessage.responseText);
+        }
+    });
+
+}
+
+
+//buscar dadosImgPorId
+function buscarDadosImgPorId(IdDadosImg) {
+    $('#IdDadosImg').css('border-color', 'lightgrey');
+    $('#FileSizeBytes').css('border-color', 'lightgrey');
+    $('#Url').css('border-color', 'lightgrey');
+    $('#Descricao').css('border-color', 'lightgrey');
+    $.ajax({
+        url: "/Home/BuscarDadosImgPorId/" + IdDadosImg,
+        type: "GET",
+        contentType: "application/json;charset=UTF-8",
+        dataType: "json",
+        success: function (result) {
+            $('#IdDadosImgEditar').val(result.IdDadosImg);
+            $('#FileSizeBytesEditar').val(result.FileSizeBytes);
+            $('#UrlEditar').val(result.Url);
+            $('#DescricaoEditar').val(result.Descricao);
+            $('#modalEditar').modal('show');
+        },
+        error: function (errormessage) {
+            alert(errormessage.responseText);
+        }
+    });
+    return false;
+}
+
+//Editar dadosImg
+
+function atualizarDadosImg() {
+
+    var dadosImgObj = {
+        IdDadosImg: $('#IdDadosImgEditar').val(),
+        FileSizeBytes: $('#FileSizeBytesEditar').val(),
+        Url: $('#UrlEditar').val(),
+        Descricao: $('#DescricaoEditar').val(),
+    };
+    $.ajax({
+        url: "/Home/UpdateDadosImg",
+        data: JSON.stringify(dadosImgObj),
+        type: "POST",
+        contentType: "application/json;charset=utf-8",
+        dataType: "json",
+        success: function (result) {
+            buscarDadosIgm();
+            alert("Dados Atualizados com Sucesso!");
+            $('#modalEditar').modal('hide');
+            loadData();
+        },
+        error: function (errormessage) {
+            alert(errormessage.responseText);
+        }
+    });
+}
+
+//deletar dadosImg
+function deletarDadosImg(id) {
+    var ans = confirm("Deseja deletar o item?");
+    if (ans) {
+        $.ajax({
+            url: "/Home/DeletarDadosImg/" + id,
+            type: "POST",
+            contentType: "application/json;charset=UTF-8",
+            dataType: "json",
+            success: function (result) {
+                BuscarDadosImg();
+            },
+            error: function (errormessage) {
+                alert(errormessage.responseText);
+            }
+        });
+    };
+
+}
+
---
+++ ProjDadosImg/ProjDadosImg/Scripts/validacoes.js
@@ -0,0 +1,143 @@
+﻿
+
+
+    function funcMostrarImg() {
+                
+        document.getElementById('btnShowImg').hidden = true;
+
+        document.getElementById('btnSalvar').hidden = false;
+        document.getElementById('btnTrocarImg').hidden = false;
+        document.getElementById('btnExcluirImg').hidden = false;
+
+        buscaImagem();
+
+    }
+
+var avisado = 0;
+function funcTrocarImg() {
+
+    
+    if (avisado == 0) { 
+      var r = confirm("Se você trocar a imagem, a descrição também mudará . Deseja continuar?");
+        if (r == true) {
+           
+           avisado = 1;
+         }
+     }
+    buscaImagem();
+}
+
+    function funcExcluirImg() {
+        location.reload();
+
+}
+
+var descricao;
+var url;
+var fileSizeBytes;
+
+    function buscaImagem() {
+
+        var auxOxhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+
+        function statusOfReport() {
+            if (auxOxhr.readyState == 4)               // Request completado.
+                buscaJson(this.responseText);     // após setado, mostra o retorno.
+        }
+        auxOxhr.onreadystatechange = statusOfReport;
+        auxOxhr.open("GET", "https://random.dog/woof.json", true);   // Se false requeste sincrono, Se true requeste assincrono.
+
+        auxOxhr.send();
+
+        function buscaJson(auxJson) {
+            var auxArr = [];
+            auxArr = JSON.parse(auxJson); 	// preenche o array com o json obtido.
+
+            var div = document.getElementById('imgDog');     // <div> padrão fixa da imagem
+            div.innerHTML = '';
+
+            // Cria elementos <div> um para url e outro para imagem
+            var divLeft = document.createElement('div');
+            divLeft.setAttribute('id','Url' );
+            divLeft.innerHTML = auxArr.url;
+
+            var img = document.createElement('img');    // Cria o elemento <img>.
+            img.src = auxArr.url;                // Url da imagem.
+            descricao = "Dados da Imagem:" + "\n" + "FileSizeBytes: " + auxArr.fileSizeBytes + "\n" + "Url: " + auxArr.url;
+            document.getElementById('Descricao').value = descricao;
+            url = auxArr.url;
+            fileSizeBytes = auxArr.fileSizeBytes;
+
+            var divRight = document.createElement('div');
+            divRight.setAttribute('style', 'border-left: solid 1px #ddd;');
+            divRight.appendChild(img);
+
+            // adiciona div dentro da div pai.
+            div.appendChild(divLeft);
+            div.appendChild(divRight);
+
+            //Se a extensão for .mp4 ou .webm busca novamente
+            var auxA = auxArr.url;
+            var auxB = auxArr.url;
+            auxA = auxA.search(".mp4");
+            auxB = auxB.search(".webm");          
+
+            if (auxA >= 1) {
+                buscaImagem();
+            }
+            if (auxB >= 1) {
+                buscaImagem();
+            }
+
+
+
+        }
+};
+
+
+function salvarNoCrud() {
+
+    salvarNoCrudAux(descricao, url, fileSizeBytes);
+    
+    var r = false;
+    r = confirm("Salvo com Sucesso! Deseja ir para o Crud DadosImg?");
+    if (r == true) {
+
+        $.ajax({
+            url: "/Home/DadosImg", success: function (result) {
+                $("#DadosImg").html(result);
+                window.location.href = "/Home/DadosImg";
+            }
+        });
+    }
+    
+       
+    
+}
+
+//Add novo DadosImg
+function salvarNoCrudAux(descricao, url, fileSizeBytes) {
+
+    var urlAux = '<a href="' + url + '">' + 'acessar' + '</a>';
+
+    var dadosImgObj = {
+        FileSizeBytes: fileSizeBytes,
+        Url: urlAux,
+        Descricao: descricao
+    };
+    $.ajax({
+        url: "/Home/AddNewDadosImg",
+        data: JSON.stringify(dadosImgObj),
+        type: "POST",
+        contentType: "application/json;charset=utf-8",
+        dataType: "json",
+        success: function (result) {
+            buscarDadosIgm();
+            $('#ModalAdicionar').modal('hide');
+        },
+        error: function (errormessage) {
+            alert(errormessage.responseText);
+        }
+    })
+}
+
